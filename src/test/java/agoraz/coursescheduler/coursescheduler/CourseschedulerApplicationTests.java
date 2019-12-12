cd110af41c227e5e790e9ca37f05ceaaef130700
Badreddine Fadel <badre.fadel@gmail.com>
2019-12-12 14:09:29 +0100

    initial commit

---
+++ src/test/java/agoraz/coursescheduler/coursescheduler/CourseschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package agoraz.coursescheduler.coursescheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
